957cf1b54790a70e6fb3258b12507115ccb43359
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-04-04 14:06:46 +0530

    Poly Demo

---
+++ EnginesDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace EnginesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hybrid hybrid = new Hybrid();
+            Diesel d = new Diesel();
+            hybrid.Engines.Add(d);
+            hybrid.Start();
+        }
+    }
+
+    interface IEngine
+    {
+        void Start();
+        void Stop();
+    }
+
+    class Diesel : IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Starting Diesel Engine");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stoping Diesel Engine");
+        }
+    }
+    class Petrol : IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Starting Petrol Engine");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stoping Petrol Engine");
+        }
+    }
+
+    class Hybrid : IEngine
+    {
+        //Petrol petrol = new Petrol();
+        //Diesel diesel = new Diesel();
+        public List<IEngine> Engines { get; set; } = new List<IEngine>();
+        public void Start()
+        {
+            foreach (var item in Engines)
+            {
+                item.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
