0686a4d6eec33c1c6615bded52a579755b416e1c
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-04-07 14:05:44 +0530

    collections

---
+++ CollectionsDemo1/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace CollectionsDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // store 10 ints
+
+            // static typed collection
+
+            //int[] numbers = new int[10];
+
+            // store n ints
+
+            // store n doubles
+
+            System.Collections.Generic.List<int> ints = new List<int>();
+
+            DynamicArray<int> numbers = new DynamicArray<int>();
+
+
+            ints.Add(1);
+
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+            numbers.Add(6);
+            numbers.Add(7);
+            numbers.Add(8);
+            numbers.Add(9);
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+            numbers.Add(6);
+            numbers.Add(7);
+            numbers.Add(8);
+            numbers.Add(9);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers.Get(i));
+            }
+
+
+
+        }
+    }
+
+    public interface IDynaicIntArray
+    {
+        void Add(int x);
+        int Get(int x);
+    }
+
+    public class DynamicArray<idontknow> //: IDynaicIntArray
+    {
+        private idontknow[] numbers = new idontknow[5];
+        private int index = 0;
+        public void Add(idontknow x)
+        {
+            if (index < numbers.Length) // full
+            {
+                idontknow[] temp = new idontknow[numbers.Length * 2];
+                Array.Copy(numbers, temp, numbers.Length);
+                numbers = temp;
+            }
+            numbers[index++] = x;
+        }
+
+        public idontknow Get(idontknow x)
+        {
+            return numbers[x];
+        }
+
+        public int Length { get { return index; } }
+    }
+
+    public class DynamicDoubleArray
+    {
+        private double[] numbers = new double[5];
+        private int index = 0;
+        public void Add(double x)
+        {
+            if (index < numbers.Length) // full
+            {
+                double[] temp = new double[numbers.Length * 2];
+                Array.Copy(numbers, temp, numbers.Length);
+                numbers = temp;
+            }
+            numbers[index++] = x;
+        }
+
+        public double Get(double x)
+        {
+            return numbers[x];
+        }
+
+        public int Length { get { return index; } }
+    }
+
+}
