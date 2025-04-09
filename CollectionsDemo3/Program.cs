5b9b8a2853c9d16271b921a06a7c2e1921f3954c
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-04-09 10:56:26 +0530

    exceptions demo

---
+++ CollectionsDemo3/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace CollectionsDemo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Queue and Stack
+
+
+            // Stack
+            // Declare
+            Stack<int> numbers = new Stack<int>();
+
+            // add
+            numbers.Push(1);
+            // read
+            int x = numbers.Peek();
+            // read and remove
+            x = numbers.Pop();
+
+            // Queue
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(x);
+            // add
+
+            // read
+            x = queue.Dequeue(); // extract and delete
+            x = numbers.Pop(); // extract
+
+            // Dictionaries
+            Dictionary<int, string> results = new Dictionary<int, string>();
+            // add
+            results.Add(111, "Pass");
+
+            // read
+
+            string resultof111 = results[111];
+
+            // store days number and day name in dictionary
+            Dictionary<int, string> days = new Dictionary<int, string>();
+            days.Add(1, "Sunday");
+            days.Add(2, "Monday");
+            days.Add(3, "Tuesday");
+            days.Add(4, "Wednesday");
+            days.Add(5, "Thrusday");
+            days.Add(6, "Friday");
+            days.Add(7, "Saturday");
+
+            int dayNumber = 3;
+            string dayname = days[dayNumber];
+
+
+            // Hashset - unique data
+            HashSet<int> uniqueNumbers = new HashSet<int>();
+
+            uniqueNumbers.Add(1);
+            uniqueNumbers.Add(1);
+            Console.WriteLine(uniqueNumbers.Count);
+
+            // store 100 unique random numbers
+            List<int> list = new List<int>();
+            list = Enumerable.Range(1, 100).ToList();
+
+            Random rnd = new Random();
+
+
+            while (uniqueNumbers.Count <= 100)
+            {
+                int randomNumber = rnd.Next(1, 1000);
+                uniqueNumbers.Add(randomNumber);
+            }
+
+        }
+    }
+}
---
+++ ExceptionsDemo/Program.cs
@@ -0,0 +1,129 @@
+﻿namespace ExceptionsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // accept two numbers, find the sum then dislay and repeat
+
+            while (true)
+            {
+                try
+                {
+                    int fno;
+                    int sno;
+                    int sum;
+                    Console.WriteLine("Enter First Number");
+                    fno = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter Second Number");
+                    sno = int.Parse(Console.ReadLine());
+
+                    //sum = fno + sno;
+                    Calculator calc = new Calculator();
+                    sum = calc.Sum(fno, sno);
+
+                    Console.WriteLine($"The sum of {fno} and {sno} is {sum}");
+
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please enter only numbers");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("Please enter smaller numbers");
+                }
+                catch (Exception ex)// when (ex is FormatException) // Cath All Block
+                {
+                    Console.WriteLine(ex.Message);
+
+                    //if (ex is FormatException) 
+                    //{
+
+                    //}
+                    //else if (ex is OverflowException) 
+                    //{
+
+                    //}
+                }
+                finally
+                {
+                    Console.WriteLine("In finally block");
+                }
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Will do a basic mathematical calculations
+    /// </summary>
+    public class Calculator
+    {
+
+        /* this is multiline comment
+         * sdfsdfasdf
+         * sdfsdfasdf
+         * sdfsdfasdf
+         * sdfasdfasdf
+         */
+
+
+
+        // should find sum of two non zero non negative and non odd numbers onlyu
+
+
+        /// <summary>
+        /// should find sum of two non zero non negative and non odd numbers only
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>sum of two numbers</returns>
+        /// <exception cref="ValueZeroInputException"></exception>
+        /// <exception cref="NegativeInputException"></exception>
+        /// <exception cref="OddNumberInputException"></exception>
+        public int Sum(int a, int b)
+        {
+            // non zero
+            if (a == 0 || b == 0)
+            {
+                throw new ValueZeroInputException("Please enter non zero numbers only");
+            }
+            if (a < 0 || b < 0)
+            {
+                throw new NegativeInputException("Please enter non negative numbers only");
+            }
+            if (a % 2 != 0 || b % 2 != 0)
+            {
+                throw new OddNumberInputException("Please enter non odd numbers only");
+            }
+            return a + b;
+        }
+    }
+
+    public class ValueZeroInputException : ApplicationException
+    {
+        public ValueZeroInputException(string msg = null) : base(msg)
+        {
+            //this.Message = msg;
+        }
+    }
+
+    public class NegativeInputException : ApplicationException
+    {
+        public NegativeInputException(string msg = null) : base(msg)
+        {
+
+        }
+    }
+
+    public class OddNumberInputException : ApplicationException
+    {
+        public OddNumberInputException(string msg = null) : base(msg)
+        {
+
+        }
+    }
+}
